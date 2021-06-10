6026b932ac3fbfc473bfed8221a48bcdea289781
Baruch <brsv11@gmail.com>
2021-06-10 23:20:39 +0300

    HW01 - before run main

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,46 @@
 /**
  * Array based storage for Resumes
  */
+//Реализуйте методы save, get, delete, clear, getAll, size
+// в классе ArrayStorage, организовав хранение резюме в массиве
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int size = 0;
 
     void clear() {
+        for (int i = 0; i < size; i++)
+            storage[i] = null;
     }
 
     void save(Resume r) {
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < size; i++)
+            if (storage[i].uuid.equals(uuid))
+                return storage[i];
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < size; i++)
+            if (storage[i].uuid.equals(uuid))
+                storage[i] = null;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] resumes = new Resume[size];
+        for (int i = 0; i < size; i++)
+            resumes[i] = storage[i];
+        return resumes;
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
